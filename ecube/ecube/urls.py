7102032da741d5bc3b4d4688fffd2dc57b5dde8f
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2018-11-20 17:24:50 -0600

    Configuration files and static files

--- ecube/ecube/urls.py
+++ ecube/ecube/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,3 +26,8 @@ urlpatterns = [
     url(r'^events/', include('events.urls', namespace='events')),
     url(r'^sponsors/', include('sponsors.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Servir archivos media
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
