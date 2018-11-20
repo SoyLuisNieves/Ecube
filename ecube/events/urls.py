8ff0c1af65289f3952c1677d0d3d2e5abaea5960
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2018-11-20 16:15:02 -0600

    Creation view to update event, event_update.html

--- ecube/events/urls.py
+++ ecube/events/urls.py
@@ -4,10 +4,12 @@ from .views import (
 	home_events,
 	detail_event,
 	create_event,
+	event_update,
 )
 
 urlpatterns = [
 	url(r'^create/$', create_event),
 	url(r'^$', home_events,name='home_events'),
 	url(r'^detail/(?P<id>[\d+]+)/', detail_event, name='detail'),
+	url(r'^update/(?P<id>[\d+]+)/', event_update, name='update'),
 ]
--- ecube/events/views.py
+++ ecube/events/views.py
@@ -5,31 +5,48 @@ from django.http import HttpResponseRedirect
 from .models import Event
 from .forms import EventForm
 
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
 
 def home_events(request):
-	queryset = Event.objects.all()
-	context = {
-		'events': queryset,
-	}
-	return render(request, 'index.html', context)
+    queryset = Event.objects.all()
+    context = {
+        'events': queryset,
+    }
+    return render(request, 'index.html', context)
 
 def detail_event(request, id):
 
-	queryset = Event.objects.get(id=id)
-	context = {
-		'event': queryset,
-	}
-	return render(request, 'detail_event.html', context)
+    queryset = Event.objects.get(id=id)
+    context = {
+        'event': queryset,
+    }
+    return render(request, 'detail_event.html', context)
 
 def create_event(request):
-	form = EventForm(request.POST or None, request.FILES or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = {
-		"form": form,
-	}
-	return render(request, 'create_form.html', context)
+    form = EventForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "form": form,
+    }
+    return render(request, 'create_form.html', context)
+
+def event_update(request, id):
+    instance = get_object_or_404(Event, id=id)
+    form = EventForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+    "title": instance.name,
+    "instance": instance,
+    "form": form
+    }
+    return render(request, "event_update.html", context)
